ab6a3ce346b56cee27f5c989c6eb64c4bf91b442
qys233 <499465013@qq.com>
2021-06-19 00:55:42 +0800

    1

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: '小明' },
+      { id: 2, name: '小红' },
+      { id: 3, name: '小王' },
+      { id: 4, name: '小雷' },
+      { id: 5, name: '老五' },
+      { id: 6, name: '老六' },
+      { id: 7, name: '老七' },
+    ];
+    return {heroes};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: '小明' },
+  { id: 2, name: '小红' },
+  { id: 3, name: '小王' },
+  { id: 4, name: '小雷' },
+  { id: 5, name: '老五' },
+  { id: 6, name: '老六' },
+  { id: 7, name: '老七' },
+];
